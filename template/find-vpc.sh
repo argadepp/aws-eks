899a5b53ba8edfc131e8fe490e74d9df12780d98
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-12-06 14:43:29 +0530

    Update find-vpc.sh

--- template/find-vpc.sh
+++ template/find-vpc.sh
@@ -17,27 +17,27 @@ subnet_list=$(jq -r '.Subnets[].SubnetId' "${WORKSPACE}/template/subnets.json")
 
 echo $appsubnets
 
+
 parameterfilepath="file://${WORKSPACE}/template/parameters.json"
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!Substitute the parameters!!!!!!!!!!!!!!!!"
-envsubst < "${WORKSPACE}/template/eks_parameters.json.tmpl}" > "${WORKSPACE}/template/parameters.json}"
+envsubst < "${WORKSPACE}/template/asg_parameters.json.tmpl" > "${WORKSPACE}/template/parameters.json"
 templateUrl="file://${WORKSPACE}/template/control-plane.yaml"
-echo "Temp File"
-cat "${WORKSPACE}/template/eks_parameters.json.tmpl"
+cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
 echo "!!!!!!!!!!!!!!!!Final Parameters!!!!!!!!!!!!!!!!"
-cat "${WORKSPACE}/template/parameters.json}"
+cat "${WORKSPACE}/template/parameters.json"
 
-echo "!!!!!!!!!!!!! ${action} of "eks-${environment}-${stackName}" stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
-aws cloudformation "${action}"-stack \
---template-body "${templateUrl}" --region "${aws_region}" \
---stack-name "eks-${environment}-${stackName}" \
---parameters  "${parameterfilepath}" \
---capabilities CAPABILITY_NAMED_IAM
+# echo "!!!!!!!!!!!!! ${action} of "eks-${environment}-${stackName}" stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
+# aws cloudformation "${action}"-stack \
+# --template-body "${templateUrl}" --region "${aws_region}" \
+# --stack-name "eks-${environment}-${stackName}" \
+# --parameters  "${parameterfilepath}" \
+# --capabilities CAPABILITY_NAMED_IAM
 
-echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
-aws cloudformation wait stack-${action}-complete \
-    --stack-name "eks-${environment}-${stackName}" \
-    --region ${aws_region}
+# echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
+# aws cloudformation wait stack-${action}-complete \
+#     --stack-name "eks-${environment}-${stackName}" \
+#     --region ${aws_region}
     
-echo "${stackName} is created successfully !!!!!!!!!!!!!!!!"   
+# echo "${stackName} is created successfully !!!!!!!!!!!!!!!!"   
 
