89aab4c25c249c792c31e6fd09f0933db62a1c07
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-12-06 15:06:19 +0530

    Update find-vpc.sh

--- template/find-vpc.sh
+++ template/find-vpc.sh
@@ -27,17 +27,17 @@ cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
 echo "!!!!!!!!!!!!!!!!Final Parameters!!!!!!!!!!!!!!!!"
 cat "${WORKSPACE}/template/parameters.json"
 
-# echo "!!!!!!!!!!!!! ${action} of "eks-${environment}-${stackName}" stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
-# aws cloudformation "${action}"-stack \
-# --template-body "${templateUrl}" --region "${aws_region}" \
-# --stack-name "eks-${environment}-${stackName}" \
-# --parameters  "${parameterfilepath}" \
-# --capabilities CAPABILITY_NAMED_IAM
+echo "!!!!!!!!!!!!! ${action} of "eks-${environment}-${stackName}" stack is initiated !!!!!!!!!!!!!!!!!!!!!!!!"
+aws cloudformation "${action}"-stack \
+--template-body "${templateUrl}" --region "${aws_region}" \
+--stack-name "eks-${environment}-${stackName}" \
+--parameters  "${parameterfilepath}" \
+--capabilities CAPABILITY_NAMED_IAM
 
-# echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
-# aws cloudformation wait stack-${action}-complete \
-#     --stack-name "eks-${environment}-${stackName}" \
-#     --region ${aws_region}
+echo "Waiting for the '${action}' operation to complete on CloudFormation stack: ${environment}-${stackName}"
+aws cloudformation wait stack-${action}-complete \
+    --stack-name "eks-${environment}-${stackName}" \
+    --region ${aws_region}
     
-# echo "${stackName} is created successfully !!!!!!!!!!!!!!!!"   
+echo "${stackName} is created successfully !!!!!!!!!!!!!!!!"   
 
