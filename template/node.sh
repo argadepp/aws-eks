ac2a0824f0068d75d1837a403185b7a97a8d5d25
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2022-12-06 16:39:11 +0530

    Update node.sh

--- template/node.sh
+++ template/node.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-cluster_endpoint=$(aws eks describe-cluster \
+APIEndPoint=$(aws eks describe-cluster \
     --region "${aws_region}" \
     --name "${ClusterName}" \
     --query "cluster.endpoint" \
     --output text)
 
-certificate_data=$(aws eks describe-cluster \
+CertData=$(aws eks describe-cluster \
     --region "${aws_region}" \
     --name "${ClusterName}" \
     --query "cluster.certificateAuthority.data" \
     --output text)
 
-echo    "endpoint=$cluster_endpoint" 
+echo    "endpoint=$APIEndPoint" 
 
-echo "cert_date=$certificate_data"
+echo "cert_date=$CertData"
+
+
+aws ec2  describe-security-groups > "${WORKSPACE}/template/sg.json"
+aws iam get-role --role-name eksDev-node-role > "${WORKSPACE}/template/iam.json"
+SG=$(jq -r '.SecurityGroups[].GroupId' "${WORKSPACE}/template/sg.json" | sed ':a;N;$!ba;s/\n/,/g')
+RoleArn=$(jq -r '.Role.Arn' "${WORKSPACE}/template/iam.json")
+
+parameterfilepath="file://${WORKSPACE}/template/nodeparameters.json"
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!Substitute the parameters!!!!!!!!!!!!!!!!"
+envsubst < "${WORKSPACE}/template/node_parameters.json.tmpl" > "${WORKSPACE}/template/nodeparameters.json"
+templateUrl="file://${WORKSPACE}/template/controle-plane.yaml"
+#cat "${WORKSPACE}/template/asg_parameters.json.tmpl"
+echo "!!!!!!!!!!!!!!!!Final Parameters!!!!!!!!!!!!!!!!"
+cat "${WORKSPACE}/template/nodeparameters.json"
+
+
+
+
+
+
 
